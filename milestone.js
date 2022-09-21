4e6272d34129982b00c60f9b1b9005ccb9c4086a
Erik Sabin <eriksabin97@gmail.com>
2022-09-21 13:06:09 -0400

    recommiting to have correct code

--- milestone.js
+++ milestone.js
@@ -15,47 +15,22 @@
 // OOP
 
 let attackTurn;
+let playButton = document.getElementById('autoplay')
 
 
-
-let player1 = {
-
-        name:'Erik',
-        stats:{
-        health: 100,
-        defense: 0,
-        attack: player1.inventory.woodSword
-        },
-        inventory:{
-        armor: armor,
-        weapon: weapons.woodSword.attack,
-        },
-    
-}
-
-
-class EnemyPlayer{
-
-    constructor(name, health, defense, armor, weapon){
-
-        this.name = name;
-        this.health = health;
-        this.defense = defense;
-        this.armor = armor;
-        this.weapon = weapon;
-
-    }
-}
 
 let weapons = {
 
     woodSword: {
+        weaponName: 'Wood Sword',
         attack: 1
     },
     ironSword: {
+        weaponName:'Iron Sword',
         attack: 2
     },
     steelSword: {
+        weaponName: 'Steel Sword',
         attack: 4
     },
     battleAxe: {
@@ -69,7 +44,7 @@ let weapons = {
     },
     swordOfInfinitePower: {
         attack: 999
-    }
+    },
 
 }
 
@@ -95,10 +70,131 @@ let armor = {
     },
     armorOfGods: {
         defense: 999
+    },
+
+}
+
+
+const player1 = {
+
+        name:'Erik',
+        inventory:{
+        armor: 0,
+        weapon: weapons.woodSword.attack,
+        weaponName: weapons.woodSword.weaponName,
+        },
+        stats:{
+            health: 100,
+            defense: 0,
+            attack: weapons.woodSword.attack
+            },
+        
+}
+
+const enemies = {
+borgor: {
+
+    
+
+        name: 'borgor',
+        stats: {
+        health: 40,
+        attack: 1,
+        defense: 0,
+        },
+        inventory: {
+        armor:0,
+        weapon: weapons.ironSword.attack,
+        weaponName: weapons.ironSword.weaponName
+        }
+
+    
+},
+Funedad : {
+
+    name: 'Funedad',
+    stats: {
+    health: 50,
+    attack: 2,
+    defense: 0,
+    },
+    inventory: {
+    armor:0,
+    weapon: weapons.steelSword.attack,
+    weaponName: weapons.steelSword.weaponName
+    }
+}
+   
+}
+let playerHealthTag = document.getElementById('playerHealthTag')
+let playerAttackTag = document.getElementById('playerAttackTag')
+let playerWeaponTag = document.getElementById('playerWeaponTag')
+
+
+let enemyHealthTag = document.getElementById('enemyHealthTag')
+let enemyAttackTag = document.getElementById('enemyAttackTag')
+let enemyWeaponTag = document.getElementById('enemyWeaponTag')
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('playerName').textContent = player1.name
+    playerHealthTag.textContent = player1.stats.health
+    playerAttackTag.textContent = player1.stats.attack
+    playerWeaponTag.textContent = player1.inventory.weaponName
+
+    document.getElementById('enemyName').textContent = enemies.borgor.name
+    enemyHealthTag.textContent = enemies.borgor.stats.health
+    enemyAttackTag.textContent = enemies.borgor.stats.attack
+    enemyWeaponTag.textContent = enemies.borgor.inventory.weaponName
+})
+
+
+let player1Attack = player1.stats.attack 
+player1Attack = player1.inventory.weapon + 2
+
+
+
+console.log(player1Attack)
+console.log(this.name)
+
+function bossfights(enemyType){
+
+
+let enemyAttack = enemyType.stats.attack
+    enemyAttack = enemyType.inventory.weapon
+
+while(player1.stats.health >= 0 && enemyType.stats.health >= 0){
+
+    if(player1.stats.health >= 0 && enemyType.stats.health >= 0){
+
+        player1.stats.health -= enemyAttack
+        enemyType.stats.health -= player1Attack
+        playerHealthTag.textContent -= enemyAttack
+        enemyHealthTag.textContent -= player1Attack
+        console.log(`The player has ${player1.stats.health} health and the enemy has ${enemyType.stats.health}`)
+
+    }
+
+    if(enemyType.stats.health <= 0){
+
+        console.log(`${player1.name} wins!!
+        Congrats! You looted an ${enemyType.inventory.weaponName}`)
+        player1.inventory.weapon = enemyType.inventory.weapon
+        player1.inventory.weaponName = enemyType.inventory.weaponName
+        player1Attack = player1.inventory.weapon + 2
+        playerWeaponTag.textContent = player1.inventory.weaponName
+        console.log(player1.inventory.weapon)
+        console.log(player1Attack)
     }
 
 }
 
+
+}
+
+
+playButton.addEventListener('click', function(){
+bossfights(enemies.borgor)
+})
 
 
 
